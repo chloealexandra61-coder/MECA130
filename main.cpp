64e95d2613a623cf627d7f370aad784b9976c0ba
chloealexandra61-coder <chloealexandra61@gmail.com>
2026-07-16 15:02:37 +1200

    Enhance main.cpp with group number and variable prints
    Added group number and additional prints to display robot and pi values.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 #include "vex.h"
-
+#define GROUP_NUMBER 35
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the IQ2 brain screen
@@ -17,15 +17,29 @@ vex::brain       Brain;
 
 
 int main() {
-	
+	int robot = 2;
+    double pi = 3.14;
     Brain.Screen.print("Hello from");
-    Brain.Screen.newLine();
+    Brain.Screen.newLine(); 
     Brain.Screen.print("Chloe and Evelynn");
-
-   
+    Brain.Screen.newLine(); 
+    Brain.Screen.print("I'm in Group %d",GROUP_NUMBER);
+    Brain.Screen.newLine(); 
+    Brain.Screen.print("%d",robot);
+    Brain.Screen.newLine(); 
+    Brain.Screen.print("%.2f",pi);
+    // Create a touchled instance in Port 10
+    touchled TouchLED10 = touchled(PORT10);
+        TouchLED10.on(50,50,50,90);
+        vex::color ledColor = vex::color(255,255,000);
+        TouchLED10.setBrightness(10);
+        TouchLED10.setBlink(ledColor,0.5,0.5);
+    
     while(1) {
         
         // Allow other tasks to run
         this_thread::sleep_for(10);
     }
+
+
 }
